2c2f7f485c90860883ff8bf1c93b53fc855bc032
James Lim <2201105872@student.buksu.edu.ph>
2025-08-24 21:25:04 +0800

    designs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,38 @@
+"use client";
+
+import BlurText from "../../components/BlurText";
+import AnimatedList from '../../components/AnimatedList'
+
+// AnimatedList
+const items = ['nigga 1', 'nigga 2', 'nigga 3', 'nigga 4', 'nigga 5', 'nigga 6', 'nigga 7', 'nigga 8', 'nigga 9', 'nigga 10']; 
+
+const handleAnimationComplete = () => {
+  console.log('Animation completed!');
+};
 
 export default function about () { 
     return(
-        <div className="flex items-center justify-center h-full">
-            <h1 className="text-3xl font-bold">about page</h1>
-        </div> 
+        <>
+        <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center h-full text-9xl font-mono mx-10">
+                <BlurText
+                    text="List of Niggas?!"
+                    delay={150}
+                    animateBy="words"
+                    direction="top"
+                    onAnimationComplete={handleAnimationComplete}
+                    className="text-2xl mb-8"
+                />
+            </div>
+
+            <AnimatedList
+                items={items}
+                onItemSelect={(item, index) => console.log(item, index)}
+                showGradients={true}
+                enableArrowNavigation={true}
+                displayScrollbar={true}
+                />
+        </div>
+        </>
     ); 
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,34 @@ import Link from 'next/link'
 import GooeyNav from '../components/GooeyNav'
 
 
+import React from "react";
+
+const letters = [
+  { char: "C", shadow: "shadow-red-500" },
+  { char: "R", shadow: "shadow-blue-500" },
+  { char: "U", shadow: "shadow-green-500" },
+  { char: "D", shadow: "shadow-yellow-500" },
+  { char: "E", shadow: "shadow-purple-500" },
+  { char: "S", shadow: "shadow-pink-500" },
+];
+
 
 export default function Home() {
-    
-
   return (
 
-    <div className="flex items-center justify-center h-full">
-      <h1 className="text-3xl font-bold">Main page</h1>
-    </div> 
+    <div className="flex justify-center items-center h-full mt-20">
+      <div className="flex gap-6">
+        {letters.map((letter, index) => (
+          <div
+            key={index}
+            className={`w-28 h-28 flex justify-center items-center rounded-2xl bg-black text-4xl font-bold shadow-lg ${letter.shadow} ${letter.hover} 
+              transition-transform duration-300 ease-in-out hover:scale-110`}
+          >
+            {letter.char}
+          </div>
+        ))}
+      </div>
+    </div>
 
 
 
